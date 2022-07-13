a5a313e1f9d831182787a417a37d1a2a58f17fb5
liticiakouam <108594256+liticiakouam@users.noreply.github.com>
2022-07-13 15:48:09 +0100

    Update AnalyticsCounter.java

--- AnalyticsCounter.java
+++ AnalyticsCounter.java
@@ -10,5 +10,24 @@ public class AnalyticsCounter {
 		//lecture des symptomes
 		ISymptomReader symptomReader = new ReadSymptomDataFromFile("symptoms.txt");
 		List<String> symptoms = symptomReader.getSymptoms();
+
+		//comptage du nombre d'occurrences de chaque symptome
+		ISymptomsCount counter = new SymptomsCounter();
+		Map<String, Integer> symptomsWithOccurrences = counter.countSymptoms(symptoms);
+
+		//ecriture dans le fichier de sortie
+		ISymptomWrite symptomWrite = new WriteSymptomDataToFile("result.out");
+		symptomWrite.writeToFile(symptomsWithOccurrences);
+
+		/*for (String symptom : symptoms) {
+			int occurrences = 1;
+			if(symptomsMap.containsKey(symptom)) {
+				occurrences = symptomsMap.get(symptom);
+				occurrences++;
+			}
+			symptomsMap.put(symptom, occurrences);
+		}*/
+
+
 	}
 }
