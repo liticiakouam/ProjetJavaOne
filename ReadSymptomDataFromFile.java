b77de6fa569ad16e24e216635127681df83751ae
liticiakouam <anzeliticia@gmail.com>
2022-07-13 13:52:00 +0100

    Comptage des symptomes

---
+++ ReadSymptomDataFromFile.java
@@ -0,0 +1,45 @@
+package com.hemebiotech.analytics;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Simple brute force implementation
+ *
+ */
+public class ReadSymptomDataFromFile implements ISymptomReader {
+
+    private final String filepath;
+
+    /**
+     *
+     * @param filepath a full or partial path to file with symptom strings in it, one per line
+     */
+    public ReadSymptomDataFromFile (String filepath) {
+        this.filepath = filepath;
+    }
+
+    @Override
+    public List<String> getSymptoms() {
+        ArrayList<String> result = new ArrayList<String>();
+
+        if (filepath != null) {
+            try {
+                BufferedReader reader = new BufferedReader (new FileReader(filepath));
+                String line = reader.readLine();
+
+                while (line != null) {
+                    result.add(line);
+                    line = reader.readLine();
+                }
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return result;
+    }
+}
